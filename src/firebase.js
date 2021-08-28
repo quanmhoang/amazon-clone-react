7b1e17a6fe94d5205fa1756afcd96a4f028d7044
Quan-MinhHoang <quan.minhhoang@coxautoinc.com>
2021-08-28 17:09:33 +0700

    First commit, add files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA1EoqaPhXaBV42Whn5DFVDBM3BycJtNAs",
+    authDomain: "clone-a7b87.firebaseapp.com",
+    projectId: "clone-a7b87",
+    storageBucket: "clone-a7b87.appspot.com",
+    messagingSenderId: "332015819700",
+    appId: "1:332015819700:web:50053b5dde71c57b2470e1",
+    measurementId: "G-CNSZMY4RY5"
+  };
